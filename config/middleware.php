b605cc6036a9b6f30f814899a8bfc949607f1eda
Deon Okonkwo <deondazy@gmail.com>
2023-12-19 22:50:19 +0100

    Update middleware for validation exceptions

--- config/middleware.php
+++ config/middleware.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 return [
     Slim\Views\TwigMiddleware::class,
-    Denosys\App\Middleware\SetFormValidationExceptionMiddleware::class,
+    Denosys\App\Middleware\SetValidationExceptionMiddleware::class,
     Denosys\App\Middleware\GetOldFormDataMiddleware::class,
-    Denosys\App\Middleware\GetFormValidationExceptionMiddleware::class,
+    Denosys\App\Middleware\GetValidationExceptionMiddleware::class,
     Denosys\App\Middleware\SessionEncryptMiddleware::class,
     Denosys\App\Middleware\SessionStartMiddleware::class,
 ];
