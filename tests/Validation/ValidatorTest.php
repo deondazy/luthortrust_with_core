365302614cc744c54777beed20f59b430f180461
Deon Okonkwo <deondazy@gmail.com>
2023-12-25 23:48:35 +0100

    Add additional validation tests

--- tests/Validation/ValidatorTest.php
+++ tests/Validation/ValidatorTest.php
@@ -50,12 +50,16 @@ test('standard validation rules', function () {
     $data = [
         'name' => '',
         'email' => 'john@example',
-        'age' => 'twenty'
+        'age' => 'twenty',
+        'too_short' => 'abc',
+        'too_long' => 'abcdefghijk',
     ];
     $rules = [
         'name' => ['required'],
         'email' => ['required', 'email'],
-        'age' => ['required', 'numeric']
+        'age' => ['required', 'numeric'],
+        'too_short' => ['min:5'],
+        'too_long' => ['max:5']
     ];
     $this->validator->validate($data, $rules);
 })->throws(ValidationException::class, 'Form validation error(s)');
@@ -122,9 +126,12 @@ test('validated returns validated data when no errors', function () {
 test('validated throws exception when errors present', function () {
     $data = ['name' => ''];
 
-    $this->validator->validate($data, ['name' => ['required']]);
-    $this->validator->validated();
-})->throws(ValidationException::class);
+
+    expect(function () use ($data) {
+        $this->validator->validate($data, ['name' => ['required']]);
+        $this->validator->validated();
+    })->toThrow(ValidationException::class);
+});
 
 test('fails returns true when validation fails', function () {
     $data = ['name' => ''];
@@ -158,4 +165,44 @@ test('errors returns empty array when validation passes', function () {
     $this->validator->validate($data, ['name' => ['required']]);
 
     expect($this->validator->errors())->toBeEmpty();
+});
+
+test('validation fails and throws exception with get errors containg all errors', function () {
+    $data = [
+        'name' => '',
+        'email' => 'john@example',
+        'age' => 'twenty',
+        'too_short' => 'abc',
+        'too_long' => 'abcdefghijk',
+    ];
+    $rules = [
+        'name' => ['required'],
+        'email' => ['required', 'email'],
+        'age' => ['required', 'numeric'],
+        'too_short' => ['min:5'],
+        'too_long' => ['max:5']
+    ];
+
+    try {
+        $this->validator->validate($data, $rules);
+    } catch (ValidationException $e) {
+        $errors = $e->getErrors();
+    }
+
+    expect($errors)->toHaveCount(5)
+        ->and($errors)->toHaveKey('name')
+        ->and($errors)->toHaveKey('email')
+        ->and($errors)->toHaveKey('age')
+        ->and($errors)->toHaveKey('too_short')
+        ->and($errors)->toHaveKey('too_long')
+        ->and($errors['name'])->not->toBeEmpty()
+        ->and($errors['name'][0])->toBe('Name is required')
+        ->and($errors['email'])->not->toBeEmpty()
+        ->and($errors['email'][0])->toBe('Email is not a valid email address')
+        ->and($errors['age'])->not->toBeEmpty()
+        ->and($errors['age'][0])->toBe('Age must be numeric')
+        ->and($errors['too_short'])->not->toBeEmpty()
+        ->and($errors['too_short'][0])->toBe('Too Short must be at least 5 characters long')
+        ->and($errors['too_long'])->not->toBeEmpty()
+        ->and($errors['too_long'][0])->toBe('Too Long must not exceed 5 characters');
 });
