fa94b3ade32d7f0d57c57a1de2ca374d68c8c8ca
Deon Okonkwo <deondazy@gmail.com>
2023-12-27 18:53:45 +0100

    Add HomeController to Frontend namespace

---
+++ app/Controllers/Frontend/HomeController.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Denosys\App\Controllers\Frontend;
+
+use Denosys\Core\Controller\AbstractController;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class HomeController extends AbstractController
+{
+    public function index(ServerRequestInterface $request): ResponseInterface
+    {
+//        dd($request);
+        return $this->view('home.index');
+    }
+
+    public function loans(): ResponseInterface
+    {
+        return $this->view('home.loans');
+    }
+
+    public function mortgage(): ResponseInterface
+    {
+        return $this->view('home.mortgage');
+    }
+
+    public function investments(): ResponseInterface
+    {
+        return $this->view('home.investments');
+    }
+
+    public function digitalServices(): ResponseInterface
+    {
+        return $this->view('home.digital-services');
+    }
+
+    public function showContactForm(): ResponseInterface
+    {
+        return $this->view('home.contact');
+    }
+
+    public function contact(): ResponseInterface
+    {
+        $this->flash('success', 'Thank you for contacting us. We will get back to you shortly.');
+        return $this->redirect('/contact');
+    }
+
+    public function privacy(): ResponseInterface
+    {
+        return $this->view('home.privacy');
+    }
+}
