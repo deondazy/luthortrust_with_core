d3014dace3fe8b95fa311d95d26bb06079986694
Deon Okonkwo <deondazy@gmail.com>
2023-12-27 18:53:51 +0100

    Add AccountController to handle account-related functionality

---
+++ app/Controllers/Frontend/Account/AccountController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Denosys\App\Controllers\Frontend\Account;
+
+use Denosys\Core\Controller\AbstractController;
+use Denosys\Core\Security\CurrentUser;
+use Psr\Http\Message\ResponseInterface;
+
+class AccountController extends AbstractController
+{
+    public function index(CurrentUser $user): ResponseInterface
+    {
+        return $this->view('account.index', ['user' => $user->getUser()]);
+    }
+
+    public function profile(): ResponseInterface
+    {
+        return $this->view('account.profile');
+    }
+}
