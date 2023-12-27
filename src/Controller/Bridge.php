7fa5b66b2f42565a7668f47f3a153cc196e39518
Deon Okonkwo <deondazy@gmail.com>
2023-12-27 18:50:32 +0100

    Add route entity binding resolver to controller invoker

--- src/Controller/Bridge.php
+++ src/Controller/Bridge.php
@@ -5,20 +5,29 @@ declare(strict_types=1);
 namespace Denosys\Core\Controller;
 
 use Denosys\Core\Http\FormRequestResolver;
+use Denosys\Core\Routing\RouteEntityBindingResolver;
 use DI\Bridge\Slim\Bridge as BaseBridge;
+use Doctrine\ORM\EntityManagerInterface;
 use Invoker\Invoker;
 use Invoker\ParameterResolver\AssociativeArrayResolver;
 use Invoker\ParameterResolver\Container\TypeHintContainerResolver;
 use Invoker\ParameterResolver\DefaultValueResolver;
 use Invoker\ParameterResolver\ResolverChain;
 use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 class Bridge extends BaseBridge
 {
     protected static function createControllerInvoker(ContainerInterface $container): ControllerInvoker
     {
         $resolvers = [
-            // Inject parameters by name first
+            // Inject route entity bindings first
+            new RouteEntityBindingResolver(
+                $container->get(EntityManagerInterface::class),
+                $container->get(ServerRequestInterface::class),
+            ),
+            // Then inject parameters by name
             new AssociativeArrayResolver(),
             // Then inject ServerRequest for FormRequest objects
             new FormRequestResolver($container),
