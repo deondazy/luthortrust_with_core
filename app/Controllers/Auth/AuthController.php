f2fd355817dad8c72f5e8b6f89fa8f87b129a3e9
Deon Okonkwo <deondazy@gmail.com>
2023-12-27 18:53:23 +0100

    Add AuthController with login, registration, and logout functionality

---
+++ app/Controllers/Auth/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Denosys\App\Controllers\Auth;
+
+use Denosys\App\Requests\Auth\LoginRequest;
+use Denosys\App\Services\UserAuthenticationService;
+use Denosys\Core\Controller\AbstractController;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface;
+
+class AuthController extends AbstractController
+{
+    public function create(): Response
+    {
+        return $this->view('auth.login');
+    }
+
+    public function login(LoginRequest $loginRequest): Response
+    {
+        $loginRequest->authenticate();
+
+        return $this->redirectToRoute('account.index');
+    }
+
+    public function showRegistrationForm(): Response
+    {
+        return $this->view('auth.register');
+    }
+
+    public function register(
+        ServerRequestInterface $request,
+        UserAuthenticationService $authenticationService
+    ): Response {
+        $formData = $request->getParsedBody();
+
+        $authenticationService->register($formData);
+
+        return $this->redirectToRoute('login');
+    }
+
+    public function forgotPassword(): Response
+    {
+        return $this->view('auth/forgot-password');
+    }
+
+    public function logout(UserAuthenticationService $authenticationService): Response
+    {
+        $authenticationService->logout();
+
+        return $this->redirectToRoute('home');
+    }
+}
