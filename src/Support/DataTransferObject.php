8c6a1c5fe90fffaec02520235c86abeeda330c0a
Deon Okonkwo <deondazy@gmail.com>
2023-12-29 12:35:17 +0100

    Add createFromArray method to DataTransferObject

--- src/Support/DataTransferObject.php
+++ src/Support/DataTransferObject.php
@@ -4,8 +4,36 @@ declare(strict_types=1);
 
 namespace Denosys\Core\Support;
 
+use ReflectionClass;
+use ReflectionException;
+
 abstract class DataTransferObject implements Arrayable
 {
+    public static function createFromArray(array $values): static
+    {
+        try {
+            $reflectionClass = new ReflectionClass(static::class);
+            $constructor = $reflectionClass->getConstructor();
+            $params = $constructor ? $constructor->getParameters() : [];
+
+            // Prepare arguments for the constructor.
+            $args = [];
+            foreach ($params as $param) {
+                $paramName = $param->getName();
+                if (array_key_exists($paramName, $values)) {
+                    $args[$paramName] = $values[$paramName];
+                } else {
+                    // Set default or null if the value is not provided.
+                    $args[$paramName] = $param->isDefaultValueAvailable() ? $param->getDefaultValue() : null;
+                }
+            }
+
+            // Instantiate the class with the constructor arguments.
+            return $reflectionClass->newInstanceArgs($args);
+        } catch (ReflectionException $e) {
+            throw new \Exception("Failed to instantiate DataTransferObject: " . $e->getMessage());
+        }
+    }
 
     /**
      * @inheritDoc
