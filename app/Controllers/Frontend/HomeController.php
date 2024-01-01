5ab657670b1eaf71e3304cda313c7870b5fe6ce1
Deon Okonkwo <deondazy@gmail.com>
2024-01-01 18:01:03 +0100

    Refactor HomeController methods to use Response alias instead of ResponseInterface

--- app/Controllers/Frontend/HomeController.php
+++ app/Controllers/Frontend/HomeController.php
@@ -5,49 +5,47 @@ declare(strict_types=1);
 namespace Denosys\App\Controllers\Frontend;
 
 use Denosys\Core\Controller\AbstractController;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface as Response;
 
 class HomeController extends AbstractController
 {
-    public function index(ServerRequestInterface $request): ResponseInterface
+    public function index(): Response
     {
-//        dd($request);
         return $this->view('home.index');
     }
 
-    public function loans(): ResponseInterface
+    public function loans(): Response
     {
         return $this->view('home.loans');
     }
 
-    public function mortgage(): ResponseInterface
+    public function mortgage(): Response
     {
         return $this->view('home.mortgage');
     }
 
-    public function investments(): ResponseInterface
+    public function investments(): Response
     {
         return $this->view('home.investments');
     }
 
-    public function digitalServices(): ResponseInterface
+    public function digitalServices(): Response
     {
         return $this->view('home.digital-services');
     }
 
-    public function showContactForm(): ResponseInterface
+    public function showContactForm(): Response
     {
         return $this->view('home.contact');
     }
 
-    public function contact(): ResponseInterface
+    public function contact(): Response
     {
         $this->flash('success', 'Thank you for contacting us. We will get back to you shortly.');
         return $this->redirect('/contact');
     }
 
-    public function privacy(): ResponseInterface
+    public function privacy(): Response
     {
         return $this->view('home.privacy');
     }
