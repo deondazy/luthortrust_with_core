a7fec437e28639bdbedac0c8cab6699a6024b328
Deon Okonkwo <deondazy@gmail.com>
2024-01-01 22:40:46 +0100

    Refactor container and add new functions

--- src/Support/Functions.php
+++ src/Support/Functions.php
@@ -2,21 +2,22 @@
 
 declare(strict_types=1);
 
-use Denosys\Core\Environment\EnvironmentLoader;
+use Denosys\Core\Application;
 use Denosys\Core\Http\RedirectResponse;
-use Denosys\Core\Support\ServiceProvider;
+use Doctrine\ORM\EntityManagerInterface;
 use Fig\Http\Message\StatusCodeInterface;
+use Denosys\Core\Environment\EnvironmentLoader;
 
-if (!function_exists('app')) {
-    function app(string $abstract = null): mixed
+if (!function_exists('container')) {
+    function container(string $abstract = null): mixed
     {
-        $app = ServiceProvider::getApplication();
+        $container = Application::getContainer();
 
         if (is_null($abstract)) {
-            return $app;
+            return $container;
         }
 
-        return $app->getContainer()->get($abstract);
+        return $container->get($abstract);
     }
 }
 
@@ -30,7 +31,7 @@ if (!function_exists('env')) {
 if (!function_exists('config')) {
     function config(string $key, mixed $default = null): mixed
     {
-        $config = app()->getConfigurations();
+        $config = container('config');
         return $config->get($key, $default);
     }
 }
@@ -42,12 +43,51 @@ if (!function_exists('redirectToRoute')) {
         array $queryParam = [],
         int $status = StatusCodeInterface::STATUS_FOUND
     ): RedirectResponse {
-        $routeUrl = app('app')
+        $routeUrl = container('app')
             ->getRouteCollector()
             ->getRouteParser()
             ->urlFor($routeName, $data, $queryParam);
         
         return new RedirectResponse($routeUrl, $status);
+    }
+}
 
+if (!function_exists('fake') && class_exists(\Faker\Factory::class)) {
+    /**
+     * Get a faker instance.
+     *
+     * @param  string|null  $locale
+     * @return \Faker\Generator
+     */
+    function fake($locale = null): \Faker\Generator
+    {
+        // TODO: Add support for locale from config
+        if (container()->has('config')) {
+            $locale ??= container()->get('config')->get('app.faker_locale');
+        }
+
+        $locale ??= 'en_US';
+
+        $abstract = \Faker\Generator::class . ':' . $locale;
+
+        if (!container()->has($abstract)) {
+            container()->set($abstract, function () use ($locale) {
+                return \Faker\Factory::create($locale);
+            });
+        }
+
+        return container()->get($abstract);
+    }
+}
+
+if (!function_exists('entityManager')) {
+    /**
+     * Get the entity manager instance.
+     * 
+     * @return \Doctrine\ORM\EntityManagerInterface
+     */
+    function entityManager(): \Doctrine\ORM\EntityManagerInterface
+    {
+        return container(EntityManagerInterface::class);
     }
 }
