a5a03e704a274f0d44a51d130a1d63491c9af931
Deon Okonkwo <deondazy@gmail.com>
2024-01-01 22:43:11 +0100

    Remove exception handler registration in index.php

--- public/index.php
+++ public/index.php
@@ -7,9 +7,6 @@ ini_set('display_errors', '1');
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Register exception handler
-$app->registerExceptionHandler();
-
 // Register middleware
 $app->registerMiddleware();
 
