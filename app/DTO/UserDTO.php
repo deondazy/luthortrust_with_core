d44a222fd8fd0904b973a7bab897f10efd32c6e3
Deon Okonkwo <deondazy@gmail.com>
2024-01-03 23:42:10 +0100

    Add UploadedFileInterface for passport photo in UserDTO

--- app/DTO/UserDTO.php
+++ app/DTO/UserDTO.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Denosys\App\DTO;
 
 use Denosys\Core\Support\DataTransferObject;
+use Psr\Http\Message\UploadedFileInterface;
 
 class UserDTO extends DataTransferObject
 {
@@ -22,7 +23,7 @@ class UserDTO extends DataTransferObject
         public readonly ?string $city,
         public readonly ?string $state,
         public readonly ?string $country,
-        public readonly ?array $passportPhoto,
+        public readonly ?UploadedFileInterface $passportPhoto,
         public readonly bool $requireCot = false,
         public readonly bool $requireImf = false,
         public readonly bool $requireTax = false,
