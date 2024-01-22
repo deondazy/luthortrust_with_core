47e3956f1b595133db73efe19d6d066f3ba53fb5
Deon Okonkwo <deondazy@gmail.com>
2024-01-22 01:42:45 +0100

    Add session handling to Request class

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -5,8 +5,58 @@ declare(strict_types=1);
 namespace Denosys\Core\Http;
 
 use Slim\Psr7\Request as SlimRequest;
+use Denosys\Core\Session\SessionInterface;
+use Denosys\Core\Session\SessionNotFoundException;
 
 class Request extends SlimRequest
 {
-    
+    /**
+     * @var SessionInterface|null
+     */
+    protected ?SessionInterface $session = null;
+
+    /**
+     * Get the session.
+     * 
+     * @return SessionInterface
+     * 
+     * @throws SessionNotFoundException
+     */
+    public function getSession(): SessionInterface
+    {
+        $session = $this->session;
+
+        if (null === $session) {
+            throw new SessionNotFoundException(
+                'The session is not available. Please add the SessionMiddleware to your application.'
+            );
+        }
+
+        return $session;
+    }
+
+    /**
+     * Check if the has a Session object.
+     * 
+     * @return bool
+     */
+    public function hasSession(): bool
+    {
+        return null !== $this->session && $this->session instanceof SessionInterface;
+    }
+
+    /**
+     * Return a new instance with the specified session object.
+     * 
+     * @param SessionInterface $session
+     * 
+     * @return void
+     */
+    public function withSession(SessionInterface $session): static
+    {
+        $clone = clone $this;
+        $clone->session = $session;
+        
+        return $clone;
+    }
 }
