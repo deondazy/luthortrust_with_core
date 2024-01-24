4185ccaac9f2c89e80750a45145c55c4e4ac794f
Deon Okonkwo <deondazy@gmail.com>
2024-01-24 19:52:40 +0100

    Add CurrentUser parameter to DashboardController index method

--- app/Controllers/Backend/DashboardController.php
+++ app/Controllers/Backend/DashboardController.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace Denosys\App\Controllers\Backend;
 
 use Denosys\Core\Controller\AbstractController;
+use Denosys\Core\Security\CurrentUser;
 use Psr\Http\Message\ResponseInterface as Response;
 
 class DashboardController extends AbstractController
 {
-    public function index(): Response
+    public function index(CurrentUser $user): Response
     {
-        return $this->view('backend.dashboard.index');
+        return $this->view('backend.dashboard.index', ['user' => $user->getUser()]);
     }
 }
