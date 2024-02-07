75a8786e192e17fdf067eb4709bb76a23452ec37
Deon Okonkwo <deondazy@gmail.com>
2024-02-07 17:10:12 +0100

    Refactor FilesystemManager class to return Filesystem instead of FilesystemInterface

--- src/Filesystem/FilesystemManager.php
+++ src/Filesystem/FilesystemManager.php
@@ -4,28 +4,32 @@ declare(strict_types=1);
 
 namespace Denosys\Core\Filesystem;
 
+use InvalidArgumentException;
+use League\Flysystem\Filesystem;
+
 class FilesystemManager
 {
-    protected $disks = [];
+    protected array $disks = [];
 
-    protected $factories = [];
+    protected array $factories = [];
 
     public function __construct(protected readonly array $filesystemConfig)
     {
     }
 
-    public function disk($name = null)
+    // TODO: return a FilesystemInterface instead of a Filesystem
+    public function disk(?string $name = null): Filesystem
     {
         $name = $name ?: $this->getDefaultDisk();
 
-        if (!isset($this->disks[$name])) {
-            $this->disks[$name] = $this->createDisk($name);
-        }
+        // if (!isset($this->disks[$name])) {
+        //     $this->disks[$name] = $this->createDisk($name);
+        // }
 
-        return $this->disks[$name];
+        return $this->disks[$name] = $this->get($name);
     }
 
-    public function extend($driver, FilesystemFactoryInterface $factory)
+    public function extend($driver, FilesystemFactoryInterface $factory): void
     {
         $this->factories[$driver] = $factory;
     }
@@ -41,7 +45,32 @@ class FilesystemManager
         return $this->factories[$config['driver']]->make($config);
     }
 
-    protected function getDefaultDisk()
+    // TODO: return a FilesystemInterface instead of a Filesystem
+    protected function get(string $name): Filesystem
+    {
+        return $this->disks[$name] ?? $this->resolve($name);
+    }
+
+    // TODO: return a FilesystemInterface instead of a Filesystem
+    protected function resolve(string $name, ?array $config = null): Filesystem
+    {
+        $config ??= $this->getConfig($name);
+
+        if (empty($config['driver'])) {
+            throw new InvalidArgumentException("Disk [$name] does not have a configured driver.");
+        }
+
+        $name = $config['driver'];
+
+        return $this->disks[$name] = $this->factories[$config['driver']]->make($config);
+    }
+
+    protected function getConfig(string $name): array
+    {
+        return $this->filesystemConfig['disks'][$name] ?: [];
+    }
+
+    protected function getDefaultDisk(): string
     {
         return $this->filesystemConfig['default'];
     }
