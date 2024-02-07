20439d7990ad49eb24d3a833d066ed832539702f
Deon Okonkwo <deondazy@gmail.com>
2024-02-07 16:59:53 +0100

    Refactor updateProfilePhoto and add deleteProfilePhoto method

--- src/Leap/HasProfilePhoto.php
+++ src/Leap/HasProfilePhoto.php
@@ -4,28 +4,40 @@ declare(strict_types=1);
 
 namespace Denosys\Core\Leap;
 
+use Denosys\Core\Filesystem\FilesystemManager;
 use Denosys\Core\Http\UploadedFile;
 use Denosys\Core\Support\Str;
 
 trait HasProfilePhoto
 {
-    public function updateProfilePhoto(UploadedFile $photo, string $storagePath = 'profile-photos'): void
+    public function updateProfilePhoto(UploadedFile $photo, string $storagePath = 'passport-photos/'): void
     {
         // $filesystem = $this->filesystemManager->disk();
 
         $originalName = $photo->getClientFilename();
         $extension = pathinfo($originalName, PATHINFO_EXTENSION);
 
-        $fileName = Str::random(40) . '.' . $extension;
+        $fileName = $storagePath . Str::random(40) . '.' . $extension;
 
         $stream = $photo->getStream()->detach();
 
-        $photo->storePublicly($storagePath . $fileName, $stream, ['disk' => 'public']);
+        $photo->storePublicly($fileName, $stream, ['disk' => 'local']);
 
         if (is_resource($stream)) {
-            $photo->getStream()->close();
+            fclose($stream);
         }
 
         $this->setPassport($fileName);
+    }
+
+    public function deleteProfilePhoto(): void
+    {
+        if (!$this->passport) {
+            return;
+        }
+
+        $filesystem = container(FilesystemManager::class)->disk('local');
+
+        $filesystem->delete($this->passport);
     }
 }
