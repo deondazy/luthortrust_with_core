5268132d6508dd4e67c025a4d61541f5d4a05b8d
Deon Okonkwo <deondazy@gmail.com>
2024-02-09 14:21:08 +0100

    Refactor Account entity relationships and add getters and setters

--- app/Database/Entities/Account.php
+++ app/Database/Entities/Account.php
@@ -6,12 +6,9 @@ namespace Denosys\App\Database\Entities;
 
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Denosys\App\Database\Entities\User;
-use Doctrine\Common\Collections\Collection;
 use Denosys\App\Repository\AccountRepository;
 use Denosys\Core\Database\Factories\HasFactory;
 use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
-use Doctrine\Common\Collections\ArrayCollection;
 use Denosys\Core\Database\Entities\Traits\HasTimestamps;
 
 #[ORM\Entity(repositoryClass: AccountRepository::class)]
@@ -27,8 +24,7 @@ class Account
     #[ORM\Column(type: Types::BIGINT, length: 20)]
     private ?int $id = null;
 
-    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'users')]
-    #[ORM\JoinColumn(name: 'user_id', referencedColumnName: 'id')]
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'accounts')]
     private ?User $user = null;
 
     #[ORM\Column(type: Types::STRING)]
@@ -47,11 +43,82 @@ class Account
     #[ORM\JoinColumn(name: 'created_by', referencedColumnName: 'id')]
     private User $createdBy;
 
-    #[ORM\OneToMany(mappedBy: 'account', targetEntity: User::class)]
-    private Collection $users;
-
-    public function __construct()
+    public function getId(): ?int
     {
-        $this->users = new ArrayCollection();
+        return $this->id;
+    }
+
+    public function setUser(User $user): Account
+    {
+        $user->addAccount($this);
+
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setNumber(string $number): Account
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    public function getNumber(): ?string
+    {
+        return $this->number;
+    }
+
+    public function setStatus(string $status): Account
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setType(string $type): Account
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setBalance(string $balance): Account
+    {
+        $this->balance = $balance;
+
+        return $this;
+    }
+
+    public function getBalance(): ?string
+    {
+        return $this->balance;
+    }
+
+    public function setCreatedBy(User $createdBy): Account
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    public function getCreatedBy(): ?User
+    {
+        return $this->createdBy;
     }
 }
