2ba7bf0ca1b5703c912d76045dac9acfabbbeed7
Deon Okonkwo <deondazy@gmail.com>
2024-02-09 14:20:57 +0100

    Refactor AccountController to include user and account data

--- app/Controllers/Backend/AccountController.php
+++ app/Controllers/Backend/AccountController.php
@@ -4,13 +4,24 @@ declare(strict_types=1);
 
 namespace Denosys\App\Controllers\Backend;
 
+use Denosys\App\Database\Entities\User;
 use Denosys\Core\Controller\AbstractController;
+use Psr\Http\Message\ResponseInterface as Response;
 
 class AccountController extends AbstractController
 {
-    public function index()
+    public function index(User $user): Response
     {
-        return $this->view('backend.accounts.index');
+        return $this->view('backend.accounts.index', [
+            'user' => $user,
+            'accounts' => $user->getAccounts(),
+        ]);
     }
 
+    public function create(User $user): Response
+    {
+        return $this->view('backend.accounts.create', [
+            'user' => $user,
+        ]);
+    }
 }
