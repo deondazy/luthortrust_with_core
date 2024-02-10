e05565d3d52cda6cd7b971810ba9249a51f0dd5e
Deon Okonkwo <deondazy@gmail.com>
2024-02-10 15:43:33 +0100

    Add store method to AccountController

--- app/Controllers/Backend/AccountController.php
+++ app/Controllers/Backend/AccountController.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Denosys\App\Controllers\Backend;
 
 use Denosys\App\Database\Entities\User;
+use Denosys\App\Requests\CreateAccountRequest;
 use Denosys\Core\Controller\AbstractController;
+use Doctrine\ORM\Exception\ORMException;
 use Psr\Http\Message\ResponseInterface as Response;
 
 class AccountController extends AbstractController
@@ -23,5 +25,17 @@ class AccountController extends AbstractController
         return $this->view('backend.accounts.create', [
             'user' => $user,
         ]);
+    }
+
+    /**
+     * @throws ORMException
+     */
+    public function store(CreateAccountRequest $accountRequest, User $user): Response
+    {
+        $accountRequest->createAccount($user);
+
+        return $this->redirectToRoute('backend.accounts.index', [
+            'user' => $user->getId(),
+        ])->withFlash('success', 'Account created successfully');
     }
 }
