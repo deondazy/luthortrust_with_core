b107b06aa03da937072b908223dd342f39753471
Deon Okonkwo <deondazy@gmail.com>
2024-02-11 20:03:36 +0100

    Refactor ControllerInvoker class to use InvokerInterface

--- src/Controller/ControllerInvoker.php
+++ src/Controller/ControllerInvoker.php
@@ -4,74 +4,39 @@ declare(strict_types=1);
 
 namespace Denosys\Core\Controller;
 
-use Slim\Psr7\Response;
-use Invoker\InvokerInterface;
-use Psr\Container\ContainerInterface;
-use Psr\Http\Message\ResponseInterface;
+use Invoker\Exception\InvocationException;
+use Invoker\Exception\NotCallableException;
+use Invoker\Exception\NotEnoughParametersException;
 use Psr\Http\Message\ServerRequestInterface;
-use DI\Bridge\Slim\ControllerInvoker as BaseControllerInvoker;
+use Slim\Interfaces\InvocationStrategyInterface;
+use Invoker\InvokerInterface;
+use Psr\Http\Message\ResponseInterface;
 
-class ControllerInvoker extends BaseControllerInvoker
+readonly class ControllerInvoker implements InvocationStrategyInterface
 {
-    private readonly InvokerInterface $invoker;
-
-    public function __construct(InvokerInterface $invoker, private readonly ContainerInterface $container)
+    public function __construct(private InvokerInterface $invoker)
     {
-        $this->invoker = $invoker;
-        parent::__construct($this->invoker);
     }
 
+    /**
+     * @throws InvocationException
+     * @throws NotCallableException
+     * @throws NotEnoughParametersException
+     */
     public function __invoke(
         callable $callable,
         ServerRequestInterface $request,
         ResponseInterface $response,
         array $routeArguments
     ): ResponseInterface {
-        if (is_array($callable) && $callable[0] instanceof AbstractController) {
-            $callable[0]->setContainer($this->container);
-        }
+        $parameters = [];
 
-        // return parent::__invoke($callable, $request, $response, $routeArguments);
-
-        // Inject the request and response by parameter name
-        $parameters = [
-            'request'  => self::injectRouteArguments($request, $routeArguments),
-            'response' => $response,
-        ];
         // Inject the route arguments by name
         $parameters += $routeArguments;
+
         // Inject the attributes defined on the request
         $parameters += $request->getAttributes();
 
-        $result = $this->invoker->call($callable, $parameters);
-
-        if ($result instanceof ResponseInterface) {
-            return $result;
-        }
-
-        $newResponse = new Response();
-        if (is_array($result) || is_object($result)) {
-            dd(json_encode($result));
-            $newResponse->getBody()->write(json_encode($result));
-            return $newResponse->withHeader('Content-Type', 'application/json');
-        }
-
-        if (is_string($result)) {
-            $newResponse->getBody()->write($result);
-            return $newResponse;
-        }
-
-        // Add additional handling for other types if necessary
-
-        return $newResponse;
-    }
-
-    private static function injectRouteArguments(ServerRequestInterface $request, array $routeArguments): ServerRequestInterface
-    {
-        $requestWithArgs = $request;
-        foreach ($routeArguments as $key => $value) {
-            $requestWithArgs = $requestWithArgs->withAttribute($key, $value);
-        }
-        return $requestWithArgs;
+        return $this->invoker->call($callable, $parameters);
     }
 }
